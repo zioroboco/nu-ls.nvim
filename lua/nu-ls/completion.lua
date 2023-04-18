d2aeacda299cfb625912a943e8c21b0367f19931
Louis Quinnell <_@ziorobo.co>
2023-04-18 22:25:30 +1000

    Very naively ask nu for completions at cursor

--- lua/nu-ls/completion.lua
+++ lua/nu-ls/completion.lua
@@ -1,3 +1,4 @@
+local helpers = require("nu-ls.helpers")
 local types = require("nu-ls.types")
 local CompletionItemKind = types.CompletionItemKind
 
@@ -5,19 +6,25 @@ local CompletionItemKind = types.CompletionItemKind
 ---@param done fun(result: CompletionList[])
 local handler = function(params, done)
 
-  ---@type CompletionList
-  local dummy_completions = {
-    isIncomplete = true,
-    items = {
-      {
-        label = "blep",
-        kind = CompletionItemKind.Text,
-      },
-    },
-  }
+  local cursor_position = helpers.get_cursor_position()
+  local cmd_string = string.format("nu --ide-complete %d %s", cursor_position, params.bufname)
+
+  local raw_nu_result = vim.fn.system(cmd_string)
+  local nu_completions = vim.fn.json_decode(raw_nu_result).completions
+
+  local items = {}
+  for _, completion in ipairs(nu_completions) do
+    table.insert(items, {
+      label = completion,
+      kind = CompletionItemKind.Text,
+    })
+  end
 
   done({
-    dummy_completions,
+    {
+      isIncomplete = true,
+      items = items,
+    },
   })
 
 end
