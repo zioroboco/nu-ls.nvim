4e40274ad9a4bff22c3884bb1fbb8ce271ff8ccd
Louis Quinnell <_@ziorobo.co>
2023-04-20 15:37:16 +1000

    Test for actual nushell commands in results

--- tests/completion_spec.lua
+++ tests/completion_spec.lua
@@ -1,3 +1,5 @@
+local any = require("fun").any
+
 local handler = require("nu-ls.completion").handler
 local spy = require("luassert.spy")
 
@@ -9,10 +11,24 @@ describe("an empty script", function()
     col = 0,
   }
 
+  local completion_items
+  local done = spy.new(function(result)
+    completion_items = result[1]["items"]
+  end)
+
+  ---@diagnostic disable-next-line
+  handler(params, done)
+
   it("returns successfully", function()
-    local done = spy.new(function() end)
-    ---@diagnostic disable-next-line
-    handler(params, done)
     assert.spy(done).was.called(1)
+  end)
+
+  it("returns nushell commands in results", function()
+    local commands_to_check = { "help", "let", "def" }
+    for _, command in ipairs(commands_to_check) do
+      assert.truthy(any(function(item)
+        return item.label == command
+      end, completion_items))
+    end
   end)
 end)
