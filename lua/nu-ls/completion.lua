e21f2fe3f858d751aecef7d4be9e5b4b72058162
Louis Quinnell <_@ziorobo.co>
2023-04-20 21:37:38 +1000

    Fix cursor position mismatch between buffers and files on disk

--- lua/nu-ls/completion.lua
+++ lua/nu-ls/completion.lua
@@ -17,8 +17,15 @@ end
 ---@param done fun(result: CompletionList[])
 local handler = function(params, done)
 
-  local cursor_position = to_absolute_position(params.row, params.col, params.bufname)
-  local cmd_string = string.format("/usr/bin/env nu --ide-complete %d %s", cursor_position, params.bufname)
+  -- we can't read the data from stdin (nu is expecting a file path), but we
+  -- also don't want to have to write our modified bufer to disk (to provide
+  -- up-to-date cursor positions, etc.) every time we want completions... so
+  -- we're making a copy of the current buffer, and pointing nu at that
+  local temp_file = vim.fn.tempname()
+  vim.fn.writefile(vim.api.nvim_buf_get_lines(params.bufnr, 0, -1, false), temp_file)
+
+  local cursor_position = to_absolute_position(params.row, params.col, temp_file)
+  local cmd_string = string.format("/usr/bin/env nu --ide-complete %d %s", cursor_position, temp_file)
 
   local raw_nu_result = vim.fn.system(cmd_string)
   local nu_completions = vim.fn.json_decode(raw_nu_result).completions
@@ -37,6 +44,9 @@ local handler = function(params, done)
       items = items,
     },
   })
+
+  -- clean up the temp file
+  vim.fn.delete(temp_file)
 
 end
 
