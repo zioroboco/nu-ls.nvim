edae8e68312708e244b311283c601e957d2b4119
Louis Quinnell <_@ziorobo.co>
2023-04-20 20:50:57 +1000

    Support completions for multi-line files

--- lua/nu-ls/completion.lua
+++ lua/nu-ls/completion.lua
@@ -1,11 +1,23 @@
 local types = require("nu-ls.types")
 local CompletionItemKind = types.CompletionItemKind
 
+-- The cursor position passed from null-ls has a 1-indexed row (line) and a
+-- 0-indexed column (think: caret position). Meanwhile, the nushell binary is
+-- instead expecting a integer number of _characters deep into the file_.
+---@param row number The 1-indexed row (line) of the cursor
+---@param col number The 0-indexed column (caret position) of the cursor
+---@param path string The path to the file
+---@return integer rv The number of characters deep the cursor is into the file
+local to_absolute_position = function(row, col, path)
+  local cmd_string = string.format("awk -v line=%d -v col=%d 'NR < line { chars += length($0) + 1 } NR == line { chars += col; print chars; exit }' %s", row, col, path)
+  return tonumber(vim.fn.system(cmd_string)) or 0
+end
+
 ---@param params Params
 ---@param done fun(result: CompletionList[])
 local handler = function(params, done)
 
-  local cursor_position = params.col
+  local cursor_position = to_absolute_position(params.row, params.col, params.bufname)
   local cmd_string = string.format("/usr/bin/env nu --ide-complete %d %s", cursor_position, params.bufname)
 
   local raw_nu_result = vim.fn.system(cmd_string)
--- tests/completion_spec.lua
+++ tests/completion_spec.lua
@@ -6,8 +6,8 @@ local spy = require("luassert.spy")
 describe("an empty script", function()
   ---@type Params
   local params = {
-    bufname = "./tests/fixtures/empty.nu",
-    row = 0,
+    bufname = "./tests/fixtures/blank.nu",
+    row = 1,
     col = 0,
   }
 
@@ -33,11 +33,45 @@ describe("an empty script", function()
   end)
 end)
 
-describe("the cursor on a single character", function()
+describe("the cursor on the first character of a one-line file", function()
   ---@type Params
   local params = {
-    bufname = "./tests/fixtures/char.nu",
-    row = 0,
+    bufname = "./tests/fixtures/one-line.nu",
+    row = 1,
+    col = 1,
+  }
+
+  local completion_items
+  local done = spy.new(function(result)
+    completion_items = result[1]["items"]
+  end)
+
+  ---@diagnostic disable-next-line
+  handler(params, done)
+
+  it("returns nushell commands starting with that character", function()
+    local commands_starting_with_l = { "ls", "let", "last", "loop", "lines", "length", "let-env", "load-env" }
+    for _, command in ipairs(commands_starting_with_l) do
+      assert.truthy(any(function(item) return item.label == command
+      end, completion_items))
+    end
+  end)
+
+  it("doesn't return nushell commands starting with other characters", function()
+    local commands_not_starting_with_l = { "help", "def" }
+    for _, command in ipairs(commands_not_starting_with_l) do
+      assert.falsy(any(function(item)
+        return item.label == command
+      end, completion_items))
+    end
+  end)
+end)
+
+describe("the cursor on the first character of the second line of a two-line file", function()
+  ---@type Params
+  local params = {
+    bufname = "./tests/fixtures/two-lines.nu",
+    row = 2,
     col = 1,
   }
 
