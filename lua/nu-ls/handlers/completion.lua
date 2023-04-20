a0f9da6c8ab0f5530a34053f00f2f2528b826d0e
Louis Quinnell <_@ziorobo.co>
2023-04-20 22:06:07 +1000

    Move cursor position fix into init, allowing tests to ignore it

--- lua/nu-ls/handlers/completion.lua
+++ lua/nu-ls/handlers/completion.lua
@@ -7,15 +7,8 @@ local to_absolute_position = require("nu-ls.common").to_absolute_position
 ---@param done fun(result: CompletionList[])
 local handler = function(params, done)
 
-  -- we can't read the data from stdin (nu is expecting a file path), but we
-  -- also don't want to have to write our modified bufer to disk (to provide
-  -- up-to-date cursor positions, etc.) every time we want completions... so
-  -- we're making a copy of the current buffer, and pointing nu at that
-  local temp_file = vim.fn.tempname()
-  vim.fn.writefile(vim.api.nvim_buf_get_lines(params.bufnr, 0, -1, false), temp_file)
-
-  local cursor_position = to_absolute_position(params.row, params.col, temp_file)
-  local cmd_string = string.format("/usr/bin/env nu --ide-complete %d %s", cursor_position, temp_file)
+  local cursor_position = to_absolute_position(params.row, params.col, params.bufname)
+  local cmd_string = string.format("/usr/bin/env nu --ide-complete %d %s", cursor_position, params.bufname)
 
   local raw_nu_result = vim.fn.system(cmd_string)
   local nu_completions = vim.fn.json_decode(raw_nu_result).completions
@@ -34,9 +27,6 @@ local handler = function(params, done)
       items = items,
     },
   })
-
-  -- clean up the temp file
-  vim.fn.delete(temp_file)
 
 end
 
--- lua/nu-ls/init.lua
+++ lua/nu-ls/init.lua
@@ -2,9 +2,23 @@ local null_ls = require("null-ls")
 
 ---@param params Params
 local handler = function(params, done)
-  if params.method == null_ls.methods.COMPLETION then
-    require("nu-ls.handlers.completion").handler(params, done)
+
+  -- we can't read the data from stdin (nu is expecting a file path), but we
+  -- also don't want to have to write our modified bufer to disk (to provide
+  -- up-to-date cursor positions, etc.), so we're writing a copy of the current
+  -- buffer to a temporary file, and pointing nu at that instead
+  params.bufname = vim.fn.tempname()
+  vim.fn.writefile(vim.api.nvim_buf_get_lines(params.bufnr, 0, -1, false), params.bufname)
+
+  local cleanup_and_done = function(result)
+    vim.fn.delete(params.bufname) -- deferred cleanup of the temp file
+    done(result)
   end
+
+  if params.method == null_ls.methods.COMPLETION then
+    require("nu-ls.handlers.completion").handler(params, cleanup_and_done)
+  end
+
 end
 
 return {
